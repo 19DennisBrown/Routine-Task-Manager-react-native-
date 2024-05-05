4b2eb65faa49bf3f5110bdab3e9d05bb74307e92
19DennisBrown <densmoney12@gmail.com>
2024-05-05 14:18:39 +0300

    First commit

---
+++ App.js
@@ -0,0 +1,74 @@
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, StyleSheet, Text, View, ScrollView } from 'react-native';
+import React, {useState} from 'react'
+import Header from './components/header';
+export default function App() {
+
+  const [todos, setTodos] = useState([
+    {text:'buy coffee', key:1},
+    {text:'create app', key:2},
+    {text:'play gts2', key:3},
+  ])
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Header/>
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.listHeader}>
+          tasks
+        </Text>
+        <View style={styles.list}>
+          <FlatList
+           numColumns={2}
+           data={todos}
+           renderItem={({ item })=>{
+            return(
+              <Text style={styles.item}>
+                {item.key}:  {item.text}
+              </Text>
+            )
+           }}
+
+          />
+        </View>
+      </View>
+        
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'grey',
+  },
+  content:{
+    alignItems:'center',
+    // backgroundColor:'yellow',
+    width:400,
+    // paddingHorizontal:10,
+    marginVertical:20,
+    marginHorizontal:'auto',
+    placeItems:'center',
+    // justifyContent:'center',
+    paddingVertical:30,
+
+  },
+  listHeader:{
+    textAlign:'center',
+    fontSize:30
+  },
+  list:{
+    marginVertical:25,
+  },
+  item:{
+    width:150,
+    borderWidth:2,
+    margin:12,
+    textAlign:'center',
+    paddingVertical:15,
+    borderRadius:8,
+  }
+});
---
+++ components/header.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+
+
+export default function Header(){
+
+  return(
+    <View style = {styles.header}>
+      <Text style={styles.title}>task manager🛺</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor:'blue',
+    paddingVertical:70,
+  },
+  title:{
+    textAlign:'center',
+    fontSize:40,
+    color:'grey',
+    fontWeight:'bold',
+    textDecorationLine:'underline'
+  }
+})
